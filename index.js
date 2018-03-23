bb56a7a23e969636dd35b1cbe4206f6b58c80143
Arjun@1234 <arjunagarwal1998@gmail.com>
2018-03-23 15:54:34 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,24 @@
+var express=require('express');
+const socket= require( 'socket.io');
+const bodyParser = require('body-parser')
+const cookieParser= require('cookie-parser');
+const app=express();
+
+app.set("views","./views");
+app.set("view engine","ejs");
+
+
+app.use('/',function(req,res,next){
+  res.render('index');
+
+});
+var server=app.listen(4000,function(err,result){
+  console.log("connected");
+});
+var io=socket(server);
+io.on('connection',function(socket){
+    socket.on('chat',function(data){
+    console.log(data);
+    io.sockets.emit('chat',data);
+  });
+});
